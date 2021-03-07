6288f21619ae2204c0d0b070b11c9f5f962bc6a5
Karina Kusumadewi <karinasarahk@gmail.com>
2021-03-07 18:49:38 +0700

    added formula for volume of a prism

--- geometry1.js
+++ geometry1.js
@@ -8,3 +8,11 @@ function BallVolumeFormula(r) {
 let r = 5;
 Answer1 = BallVolumeFormula(r);
 console.log("If the radius 'R' of the ball is " + r + " cm" + ", then its volume must be " + Answer1 + " cm^3.");
+
+// function to calculate the volume of a prism
+
+function PrismVolumeFormula(x, y, h) {
+    let Base = x * y / 2;
+    let PrismV = Base * h;
+    return PrismV;
+}
