4c250703106cfd8d9128de1f7209b0aff8e0a2f5
Karina Kusumadewi <karinasarahk@gmail.com>
2021-03-08 21:07:26 +0700

    uploaded boolean file

---
+++ Week 2/booleanExpressions.js
@@ -0,0 +1,12 @@
+// Equal Operators
+let a = "1" == 2; // false
+let b = "1" != 2; // true
+console.log(a);
+
+// Strict Equal
+let c = "1" === 1; // false
+
+// Less Strict Equal
+
+let d = "1" == 1;
+console.log(d);
