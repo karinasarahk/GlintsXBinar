1c487a433f5d4cca31b0687a7a3d922e1c03576e
Karina Kusumadewi <karinasarahk@gmail.com>
2021-03-16 19:31:50 +0700

    add geometry.js

---
+++ Week 3/Day-12/Geometry/geometry.js
@@ -0,0 +1,13 @@
+class Geometry {
+    constructor(name, type) {
+      this.name = name;
+      this.type = type;
+    }
+  
+    introduce() {
+      console.log("Hello, I'm Geometry");
+    }
+  }
+  
+  module.exports = Geometry;
+  
