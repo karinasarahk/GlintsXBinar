cdf596564fe250be90ddec31332765a41c84993b
Karina Kusumadewi <karinasarahk@gmail.com>
2021-03-17 21:44:44 +0700

    add index file

---
+++ Week 3/Day-13/geometry/index.js
@@ -0,0 +1,11 @@
+const Square = require("./square");
+const Rectangle = require("./rectangle");
+const Triangle = require("./triangle");
+
+const Beam = require("./beam");
+const Cube = require("./cube");
+const Tube = require("./tube");
+const Cone = require("./cone");
+
+
+module.exports = { Square, Rectangle, Triangle, Beam, Cube, Tube, Cone};
